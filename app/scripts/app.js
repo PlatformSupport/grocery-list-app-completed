ec0c130e8539d2798879bd8e15877fcabadee15c
Anton Dobrev <anton.dobrev@telerik.com>
2014-12-06 17:46:19 +0000

    Finished tutorial.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,93 @@
 (function () {
     var apiKey = "YOUR_API_KEY";
     var scheme = 'https';
-    
+
     var el = new Everlive({
         apiKey: apiKey,
         scheme: scheme
+    });
+
+    window.loginView = kendo.observable({
+        submit: function () {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            
+            el.Users.login(this.username, this.password,
+                function (data) {
+                    window.location.href = "#list";
+               
+                    initListView();                 
+                }, function () {
+                    navigator.notification.alert("Unfortunately we could not find your account.");
+                });
+        }
+    });
+
+    window.registerView = kendo.observable({
+        submit: function () {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, {
+                    Email: this.email
+                },
+                function () {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function () {
+                    navigator.notification.alert("Unfortunately we were unable to create your account.");
+                });
+        }
+    });
+
+    window.passwordView = kendo.observable({
+        submit: function () {
+            if (!this.email) {
+                navigator.notification.alert("Email address is required.");
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    Email: this.email
+                }),
+                success: function () {
+                    navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+                    window.location.href = "#login";
+                },
+                error: function () {
+                    navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
+                }
+            });
+        }
+    });
+
+    window.listView = kendo.observable({
+        logout: function (event) {
+            // Prevent going to the login page until the login call processes.
+            event.preventDefault();
+            el.Users.logout(function () {
+                this.loginView.set("username", "");
+                this.loginView.set("password", "");
+                window.location.href = "#login";
+            }, function () {
+                navigator.notification.alert("Unfortunately an error occurred logging out of your account.");
+            });
+        }
     });
 
     window.addView = kendo.observable({
@@ -26,37 +109,27 @@
         }
     });
 
-    var groceryDataSource = new kendo.data.DataSource({
-        offlineStorage: "grocery-storage",
-        type: "everlive",
-        transport: {
-            typeName: "Groceries"
-        }
-    });
-
-    function bindNetworkEvents() {
-        document.addEventListener("online", function() {
-            groceryDataSource.online(true);
+    function initListView() {
+        var groceryDataSource = new kendo.data.DataSource({
+            type: "everlive",
+            transport: {
+                typeName: "Groceries"
+            }
         });
         
-        document.addEventListener("offline", function() {
-            groceryDataSource.online(false);
-        }); 
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
     };
-    
+
     function initialize() {
         var app = new kendo.mobile.Application(document.body, {
             skin: "flat",
             transition: "slide"
         });
-        navigator.splashscreen.hide();
 
-        $("#grocery-list").kendoMobileListView({
-            dataSource: groceryDataSource,
-            template: "#: Name #"
-        });
-        
-        bindNetworkEvents();
+        navigator.splashscreen.hide();
     }
 
     document.addEventListener("deviceready", initialize);
